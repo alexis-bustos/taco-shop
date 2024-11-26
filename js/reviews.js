3af4f8e1b5ecb8d8714f298457d0e499331e6ac3
Alexisb710 <alexisbm710@gmail.com>
2024-11-25 22:37:35 -0800

    update createClient CDN link

--- js/reviews.js
+++ js/reviews.js
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
